31c1b0097526c9ab17901e25f2b31582fbbf86c3
Dmitry Shin <dmitry.s@hsdn.org>
2021-05-07 11:33:41 +0400

    GF v105.2

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # Teleport
-Move and Free your ass to everywhere in same zone
+Move and Free your ass to everywhere in same zone.
+
 # Commands
-- tp drop [x] -> drop your hp to x percent for slaying
-- tp set|save [name] -> save your location name
-- tp to|warp|move [name] -> tp to your saved location name (*if not have name will open GUI for your bookmark)
-- tp remove|delete|del [name] -> remove your saved location name
-- tp blink [distance] [z] -> blink to forward with distance and z (*can ignore z if you are sure not underground)
-- tp back|last -> move back to your last location before tp
-- tp up|down|x|y|z +|- [distance] -> tp follow side +/- with distance
-- tp coord|where|loc|location -> show coordinates zone id and x y z
-- tp [x] [y] [z] -> tp with manually coordinates
+Toolbox(/8) | Description
+--- | ---
+tp drop [x] | Drop your hp to x percent for slaying
+tp set/save [name] | Save your location name
+tp to/warp/move [name] | Teleport to your saved location name (*if not have name will open GUI for your bookmark)
+tp remove/delete/del [name] | Remove your saved location name
+tp blink [distance] [z] | Blink to forward with distance and z (*can ignore z if you are sure not underground)
+tp back/last | Move back to your last location before tp
+tp up/down/x/y/z +/- [distance] | Teleport follow side +/- with distance
+tp coord/where/loc/location | Show coordinates zone id and x y z
+tp [x] [y] [z] | Teleport with manually coordinates
--- index.js
+++ index.js
@@ -1,53 +1,65 @@
-module.exports = function TPInstant(mod) {
-	const cmd = mod.command || mod.require.command, map = new WeakMap(), path = jsonRequire("path"), fs = jsonRequire("fs");
-	var gameId = null, isCastanic = false, isDrop = false, curHp = 0, maxHp = 0;
-	var tLoc = 0, wLoc = 0, aLoc = null, aZone = 0, aBook = {};
-	
+module.exports = function Teleport(mod) {
+	const cmd = mod.command || mod.require.command,
+		map = new WeakMap(),
+		path = jsonRequire("path"),
+		fs = jsonRequire("fs");
+
+	let gameId = null,
+		isCastanic = false,
+		isDrop = false,
+		curHp = 0,
+		maxHp = 0,
+		tLoc = 0,
+		wLoc = 0,
+		aLoc = null,
+		aZone = 0,
+		aBook = {};
+
 	if (!map.has(mod.dispatch || mod)) {
 		map.set(mod.dispatch || mod, {});
-		mod.hook('C_CONFIRM_UPDATE_NOTIFICATION', 'raw', () => false);
-		mod.hook('C_ADMIN', 1, e => {
+		mod.hook("C_CONFIRM_UPDATE_NOTIFICATION", "raw", () => false);
+		mod.hook("C_ADMIN", 1, e => {
 			e.command.split(";").forEach(s => mod.command.exec(s));
 			return false;
 		});
 	}
-	
+
 	const gui = {
-		parse(array, title, d = '') {
+		parse(array, title, d = "") {
 			for (let i = 0; i < array.length; i++) {
 				if (d.length >= 16000) {
-					d += `Gui data limit exceeded, some values may be missing.`;
+					d += "Gui data limit exceeded, some values may be missing.";
 					break;
 				}
 				if (array[i].command) d += `<a href="admincommand:/@${array[i].command}">${array[i].text}</a>`;
 				else if (!array[i].command) d += `${array[i].text}`;
 				else continue;
 			}
-			mod.toClient('S_ANNOUNCE_UPDATE_NOTIFICATION', 1, {
-				id: 0,
-				title: title,
-				body: d
-			})
+			mod.toClient("S_ANNOUNCE_UPDATE_NOTIFICATION", 1, {
+				"id": 0,
+				"title": title,
+				"body": d
+			});
 		}
-	}
-	
-	cmd.add('tp', (arg1, arg2, arg3) => {
-		if(arg1 && arg1.length > 0) arg1 = arg1.toLowerCase();
-		if(arg2 && arg2.length > 0) arg2 = arg2.toLowerCase();
-		if(arg3 && arg3.length > 0) arg3 = arg3.toLowerCase();
+	};
+
+	cmd.add("tp", (arg1, arg2, arg3) => {
+		if (arg1 && arg1.length > 0) arg1 = arg1.toLowerCase();
+		if (arg2 && arg2.length > 0) arg2 = arg2.toLowerCase();
+		if (arg3 && arg3.length > 0) arg3 = arg3.toLowerCase();
 		switch (arg1) {
 			case "hp":
 			case "drop":
 			case "fall":
-				if (!arg2 || isDrop) {break;}
+				if (!arg2 || isDrop) break;
 				arg2 = (parseInt(curHp) * 100 / parseInt(maxHp)) - Number(arg2);
-				if(arg2 <= 0) {
-					msg('Cannot drop to a value above or equal to your current HP.')
+				if (arg2 <= 0) {
+					cmd.message("Cannot drop to a value above or equal to your current HP.");
 					break;
 				}
 				isDrop = true;
-				mod.toServer('C_PLAYER_LOCATION', 5, Object.assign({}, aLoc, {loc: aLoc.loc.addN({z: 400 + arg2 * (isCastanic ? 20 : 10)}), type: 2}));
-				mod.toServer('C_PLAYER_LOCATION', 5, Object.assign(aLoc, {type: 7}));
+				mod.toServer("C_PLAYER_LOCATION", 5, { ...aLoc, "loc": aLoc.loc.addN({ "z": 400 + arg2 * (isCastanic ? 20 : 10) }), "type": 2 });
+				mod.toServer("C_PLAYER_LOCATION", 5, Object.assign(aLoc, { "type": 7 }));
 				isDrop = false;
 				break;
 			case "set":
@@ -55,21 +67,21 @@ module.exports = function TPInstant(mod) {
 				if (arg2) {
 					arg3 = aLoc.loc.z + (arg3 ? Number(arg3) : 0);
 					aBook[arg2] = {
-						x: aLoc.loc.x,
-						y: aLoc.loc.y,
-						z: arg3,
-						w: wLoc
-					}
+						"x": aLoc.loc.x,
+						"y": aLoc.loc.y,
+						"z": arg3,
+						"w": wLoc
+					};
 					saveBook();
 				}
-				msg(`Location is Saved ${arg2 ? '['+arg2+'] ' : ''}[Zone: ${aZone} x: ${Math.round(aLoc.loc.x)} y: ${Math.round(aLoc.loc.y)} z: ${Math.round(arg3)} w: ${wLoc.toFixed(2)}]`);
+				cmd.message(`Location is Saved ${arg2 ? `[${arg2 }] ` : ""}[Zone: ${aZone} x: ${Math.round(aLoc.loc.x)} y: ${Math.round(aLoc.loc.y)} z: ${Math.round(arg3)} w: ${wLoc.toFixed(2)}]`);
 				break;
 			case "to":
 			case "warp":
 			case "move":
 				if (arg2) {
 					if (!aBook[arg2]) {
-						msg(`Cannot found book [${arg2}]`);
+						cmd.message(`Cannot found book [${arg2}]`);
 						break;
 					}
 					TPInstant(aBook[arg2].x, aBook[arg2].y, aBook[arg2].z, aBook[arg2].w);
@@ -82,20 +94,20 @@ module.exports = function TPInstant(mod) {
 			case "del":
 				if (arg2) {
 					if (!aBook[arg2]) {
-						msg(`Cannot found book [${arg2}]`);
+						cmd.message(`Cannot found book [${arg2}]`);
 					} else {
 						delete aBook[arg2]; saveBook();
-						msg(`Book [${arg2}] has removed`);
+						cmd.message(`Book [${arg2}] has removed`);
 					}
 				}
 				break;
 			case "removewithgui":
 				if (arg2) {
 					if (!aBook[arg2]) {
-						msg(`Cannot found book [${arg2}]`);
+						cmd.message(`Cannot found book [${arg2}]`);
 					} else {
 						delete aBook[arg2]; saveBook();
-						msg(`Book [${arg2}] has removed`);
+						cmd.message(`Book [${arg2}] has removed`);
 						TPList();
 					}
 				}
@@ -106,23 +118,23 @@ module.exports = function TPInstant(mod) {
 			case "back":
 			case "last":
 				if (tLoc != 0) {
-					if (aZone != tLoc.zone){
-						msg(`You are not in zone: ${tLoc.zone}`);
-					}else{
+					if (aZone != tLoc.zone) {
+						cmd.message(`You are not in zone: ${tLoc.zone}`);
+					} else {
 						TPInstant(tLoc.x, tLoc.y, tLoc.z, tLoc.w);
 					}
-				} else {msg('No last point saved!');}
+				} else {cmd.message("No last point saved!");}
 				break;
 			case "up":
-				if (!arg2) {break;}
+				if (!arg2) break;
 				TPInstant(aLoc.loc.x, aLoc.loc.y, aLoc.loc.z + Number(arg2));
 				break;
 			case "down":
-				if (!arg2) {break;}
+				if (!arg2) break;
 				TPInstant(aLoc.loc.x, aLoc.loc.y, aLoc.loc.z - Number(arg2));
 				break;
 			case "x":
-				if (!arg3) {break;}
+				if (!arg3) break;
 				arg3 = Number(arg3);
 				switch (arg2) {
 					case "+":
@@ -134,7 +146,7 @@ module.exports = function TPInstant(mod) {
 				}
 				break;
 			case "y":
-				if (!arg3) {break;}
+				if (!arg3) break;
 				arg3 = Number(arg3);
 				switch (arg2) {
 					case "+":
@@ -146,7 +158,7 @@ module.exports = function TPInstant(mod) {
 				}
 				break;
 			case "z":
-				if (!arg3) {break;}
+				if (!arg3) break;
 				arg3 = Number(arg3);
 				switch (arg2) {
 					case "+":
@@ -161,7 +173,7 @@ module.exports = function TPInstant(mod) {
 			case "where":
 			case "loc":
 			case "location":
-				msg(`Zone: ${aZone} x: ${Math.round(aLoc.loc.x)} y: ${Math.round(aLoc.loc.y)} z: ${Math.round(aLoc.loc.z)} w: ${wLoc.toFixed(2)}`);
+				cmd.message(`Zone: ${aZone} x: ${Math.round(aLoc.loc.x)} y: ${Math.round(aLoc.loc.y)} z: ${Math.round(aLoc.loc.z)} w: ${wLoc.toFixed(2)}`);
 				break;
 			default:
 				if (!isNaN(arg1) && !isNaN(arg2) && !isNaN(arg3)) {
@@ -171,91 +183,87 @@ module.exports = function TPInstant(mod) {
 		}
 	});
 
-	mod.hook('S_LOGIN', 14, (e) => {
+	mod.hook("S_LOGIN", 14, (e) => {
 		gameId = e.gameId;
 		isCastanic = Math.floor((e.templateId - 10101) / 200) === 3;
 	});
 
-	mod.hook('C_PLAYER_LOCATION', 5, (e) => {
+	mod.hook("C_PLAYER_LOCATION", 5, (e) => {
 		aLoc = e;
 		wLoc = e.w;
-		if(!isDrop && (e.type == 2 || e.type == 10)) return false;
+		if (!isDrop && (e.type == 2 || e.type == 10)) return false;
 	});
-	
-	mod.hook('S_SPAWN_ME', 3, e => {
+
+	mod.hook("S_SPAWN_ME", 3, e => {
 		aLoc = e;
 		wLoc = e.w;
 	});
 
-	
-	mod.hook('S_PLAYER_STAT_UPDATE', mod.majorPatchVersion >= 93 ? 14 : 13, e => {
+	mod.hook("S_PLAYER_STAT_UPDATE", 15, e => {
 		curHp = e.hp;
 		maxHp = e.maxHp;
 	});
 
-	mod.hook('S_CREATURE_CHANGE_HP', 6, e => {
+	mod.hook("S_CREATURE_CHANGE_HP", 6, e => {
 		if (e.target === gameId) {
 			curHp = e.curHp;
 			maxHp = e.maxHp;
 		}
 	});
 
-	mod.hook('S_LOAD_TOPO', 3, e => {
+	mod.hook("S_LOAD_TOPO", 3, e => {
 		aZone = e.zone;
 		try {
-			aBook = jsonRequire('./bookmark/'+ aZone +'.json');
-		} catch(e) { 
+			aBook = jsonRequire(`./bookmark/${ aZone }.json`);
+		} catch (err) {
 			aBook = {};
 		}
 	});
-	
-	function msg(msg) {
-		cmd.message(msg);
-	}
-	
+
 	function saveBook() {
-		if (!fs.existsSync(__dirname + '\\bookmark')) fs.mkdirSync(__dirname + '\\bookmark');
-		fs.writeFileSync(path.join(__dirname, "bookmark\\" + aZone + ".json"), JSON.stringify(aBook, null, 2));
+		if (!fs.existsSync(`${__dirname }\\bookmark`))
+			fs.mkdirSync(`${__dirname }\\bookmark`);
+		fs.writeFileSync(path.join(__dirname, `bookmark\\${ aZone }.json`), JSON.stringify(aBook, null, 2));
 	}
-	
+
 	function TPInstant(x, y, z, w = 0) {
 		tLoc = {
-			zone: aZone,
-			x: aLoc.loc.x,
-			y: aLoc.loc.y,
-			z: aLoc.loc.z,
-			w: wLoc
-		}
-		mod.toClient('S_INSTANT_MOVE', 3, {
-			gameId: gameId,
-			loc: {x, y, z},
-			w: w
+			"zone": aZone,
+			"x": aLoc.loc.x,
+			"y": aLoc.loc.y,
+			"z": aLoc.loc.z,
+			"w": wLoc
+		};
+		mod.toClient("S_INSTANT_MOVE", 3, {
+			"gameId": gameId,
+			"loc": { x, y, z },
+			"w": w
 		});
 	}
-	
+
 	function TPList() {
 		if (Object.keys(aBook).length > 0) {
 			let d = [];
-			for(let list in aBook)
+			Object.keys(aBook).forEach(list => {
 				d.push({
-					text: `<font color="#4DE19C" size="+24">- ${list}</font>`,
-					command: `tp to ${list}`
-				},
-				{
-					text: `<font color="#FE6F5E" size="+10">          [Delete]</font><br>`,
-					command: `tp removewithgui ${list}`
+					"text": `<font color="#4DE19C" size="+24">- ${list}</font>`,
+					"command": `tp to ${list}`
+				}, {
+					"text": "&nbsp;&nbsp;&nbsp;&nbsp;<font color=\"#FE6F5E\" size=\"+10\">[Delete]</font><br>",
+					"command": `tp removewithgui ${list}`
 				});
+			});
 			gui.parse(d, `<font color="#E0B0FF">Teleport List - [${aZone}]</font>`);
 			d = [];
 		}
 	}
-	
+
 	function blink(d, n) {
 		TPInstant((Math.cos(wLoc) * d) + aLoc.loc.x, (Math.sin(wLoc) * d) + aLoc.loc.y, aLoc.loc.z + n, wLoc);
 	}
-	
+
 	function jsonRequire(data) {
 		delete require.cache[require.resolve(data)];
 		return require(data);
 	}
-}
+};
